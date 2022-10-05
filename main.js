5d706a29cccf39d99c13528362dff270ae964681
Armand WADJI <armand_wadji@yahoo.fr>
2022-10-05 17:35:44 +0200

    add the design systeme in css

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const toggleMenu = document.querySelector(".toggle-menu");
-const menuContainer = document.querySelector(".menu-container");
+const menuContainer = document.querySelector(".main-header");
+const heroTitle = document.querySelector(".page-title");
+
+const menu = document.querySelector(".menu");
 
 toggleMenu.addEventListener("click", () => {
-  menuContainer.classList.toggle("show");
+  console.log("click");
 
   if (menuContainer.classList.contains("show")) {
+    menuContainer.classList.toggle("show");
+    heroTitle.classList.toggle("show");
+    menu.classList.toggle("show");
     toggleMenu.firstElementChild.attributes.src.value =
       "./img/icon-hamburger.svg";
   } else {
+    menuContainer.classList.toggle("show");
+    heroTitle.classList.toggle("show");
+    menu.classList.toggle("show");
     toggleMenu.firstElementChild.attributes.src.value = "./img/icon-close.svg";
   }
 });
